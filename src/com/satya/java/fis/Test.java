39f398733af4316dcaca03834dee5ecf2cbf7a1a
user <user@DESKTOP-U6HT6ET>
2020-02-16 09:47:56 +0530

    Committing Test of fis int branch x

--- src/com/satya/java/fis/Test.java
+++ src/com/satya/java/fis/Test.java
@@ -3,7 +3,7 @@ package com.satya.java.fis;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("just for testing");
+		System.out.println("Just for testing");
 
 	}
 
